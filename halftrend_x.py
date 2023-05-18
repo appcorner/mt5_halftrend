4b197b2c3e68ba71175f6c6f4f965f2baa385965
anusornp <anusornp@revolictech.com>
2023-05-18 17:29:13 +0700

    revise code & sample config

--- halftrend_x.py
+++ halftrend_x.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import asyncio
 import MetaTrader5 as mt5
 import pandas as pd
--- stupid_share.py
+++ stupid_share.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 from datetime import datetime
 from enum import Enum
